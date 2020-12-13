0f801ddfc98280c2b9a2b3b53cba9e0a02888d2c
armoniaYbienestar <70145207+armoniaYbienestar@users.noreply.github.com>
2020-12-12 22:33:01 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
+const hamburguesa = document.querySelector('.hamburguesa');
+const menunavegacionmenu = document.querySelector('.menu-navegacion__menu');
+
 window.addEventListener('scroll', function(){
     var cabezal = document.querySelector('nav');
     cabezal.classList.toggle('sticky', window.scrollY > 0); 
  });
 
  $('.menu-navegacion__boton').click(function(){
-     $('.menu-navegacion .menu-navegacion__menu').toggleClass("active");
+     $('.menu-navegacion__menu').toggleClass("active");
     
-     $('.menu-navegacion__boton i').toggleClass("active");
+     $('.hamburguesa').toggleClass("active");
+ });
+
+ window.addEventListener('click', e=>{
+    if(menunavegacionmenu.classList.contains('active'))
+    {
+        $('.hamburguesa').toggleClass(".menu-navegacion-menu__ocultar");
+    }
  });
